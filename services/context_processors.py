2b6462c4efedd3aa9aee31d0217d613febdfc2b0
Wendy-Tabitha <wendytbt4@gmail.com>
2025-05-03 18:26:08 +0300

    Implement dynamic service listing in navbar and add service request button in customer profile

---
+++ services/context_processors.py
@@ -0,0 +1,8 @@
+from .models import Service
+
+def available_service_fields(request):
+    # Get all fields that have at least one service
+    fields = Service.objects.values_list('field', flat=True).distinct()
+    return {
+        'available_fields': fields
+    } 
