03c3390718ed2baf27f74c28888fe1b55bbc1756
Wendy-Tabitha <wendytbt4@gmail.com>
2025-05-04 22:17:16 +0300

    Enhance service creation form to dynamically set field choices based on company type

--- services/forms.py
+++ services/forms.py
@@ -18,15 +18,31 @@ class CreateNewService(forms.Form):
         widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter price per hour'})
     )
     field = forms.ChoiceField(
-        choices=Service.FIELD_CHOICES,
+        choices=(),  # Will be set in __init__
         required=True,
         widget=forms.Select(attrs={'class': 'form-control'})
     )
 
     def __init__(self, *args, **kwargs):
+        company_field = kwargs.pop('company_field', None)
         super().__init__(*args, **kwargs)
-        # Set the initial choices for the field dropdown
-        self.fields['field'].choices = [('', 'Select a service field')] + list(Service.FIELD_CHOICES)
+        
+        # Set field choices based on company type
+        if company_field == 'All in One':
+            # If company is All in One, show all field choices
+            self.fields['field'].choices = [('', 'Select a service field')] + list(Service.FIELD_CHOICES)
+        else:
+            # If company has a specific field, only show that field
+            self.fields['field'].choices = [(company_field, company_field)]
+            # Set the initial value to the company's field
+            self.initial['field'] = company_field
+            # Make the field read-only for non-All in One companies
+            self.fields['field'].widget.attrs['disabled'] = 'disabled'
+            # Add a hidden input to ensure the field value is submitted
+            self.fields['field'].widget.attrs['name'] = 'field'
+            self.fields['field'].widget.attrs['value'] = company_field
+            # Add a hidden input to ensure the field value is submitted
+            self.fields['field'].widget.attrs['style'] = 'background-color: #f8f9fa;'
 
 
 class RequestServiceForm(forms.Form):
